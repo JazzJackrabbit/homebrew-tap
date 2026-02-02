995079070c0190d549efc9d26258b92654e561a5
Kirill Ragozin <ragozin@hey.com>
2026-02-02 07:16:54 +0300

    chore: update ihistory to v0.1.1

--- Formula/ihistory.rb
+++ Formula/ihistory.rb
@@ -5,48 +5,43 @@
 class Ihistory < Formula
   desc 'A minimal, fast, fuzzy shell history search tool'
   homepage 'https://github.com/jazzjackrabbit/ihistory'
-  version '0.1.0'
+  version '0.1.1'
   license 'MIT'
 
   on_macos do
     on_arm do
       url "https://github.com/jazzjackrabbit/ihistory/releases/download/v#{version}/ihistory-aarch64-apple-darwin.tar.gz"
-      sha256 '8ab9d49eb8afe49180309ff7bd80bb8d657adb7dded1af9ae7d6cf17c89e84e2'
+      sha256 'af71ee7460d7532a44338c20f93901fcf9dcc4987725258863819608d329a99b'
     end
 
     on_intel do
       url "https://github.com/jazzjackrabbit/ihistory/releases/download/v#{version}/ihistory-x86_64-apple-darwin.tar.gz"
-      sha256 '148f0521bc2bc8b7d29e4596b7b07053743e1f388ae9d1d3cd34a8fb325af396'
+      sha256 'e2279b77995bf65add4434f088bbaaf55fbc89293284de303678043bfb808f6b'
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/jazzjackrabbit/ihistory/releases/download/v#{version}/ihistory-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 '4521c66ba6cc1c6c016b037002ee5ffe1dc65ef2f9d382d2c6e1b22cd2c4dfb3'
+      sha256 'a9aaa35eaa97cfd94d46485ffab5f533b9a72a4ddf20759ee2ce8b9d7b59ab2b'
     end
 
     on_intel do
       url "https://github.com/jazzjackrabbit/ihistory/releases/download/v#{version}/ihistory-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 '77f35161b65474c5f687e876f42cafd91e67c7e1c75ebde43ec67570ae54b2f5'
+      sha256 'c7f3d62edcdf16c03be2a6f707562edce41193ec2c9e38476da4482f2b7b8203'
     end
   end
 
   def install
     bin.install 'ihistory'
     bin.install_symlink 'ihistory' => 'ih'
-    (share / 'ihistory').install Dir['shell/*']
+    (share / 'ihistory').install 'shell/ihistory.sh'
   end
 
   def caveats
     <<~EOS
-      Add to your shell config:
-
-      For zsh (~/.zshrc):
-        source "#{opt_share}/ihistory/ihistory.zsh"
-
-      For bash (~/.bashrc):
-        source "#{opt_share}/ihistory/ihistory.bash"
+      Add to your ~/.zshrc or ~/.bashrc:
+        source "#{opt_share}/ihistory/ihistory.sh"
     EOS
   end
 
