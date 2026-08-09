89ce798340c3fc28fe194210cce2baac22ce8a6a
Kirill Ragozin <ragozin@hey.com>
2026-08-09 19:21:43 +0900

    ihistory 0.1.5

--- Formula/ihistory.rb
+++ Formula/ihistory.rb
@@ -5,30 +5,30 @@
 class Ihistory < Formula
   desc 'A minimal, fast, fuzzy shell history search tool'
   homepage 'https://github.com/jazzjackrabbit/ihistory'
-  version '0.1.4'
+  version '0.1.5'
   license 'MIT'
 
   on_macos do
     on_arm do
       url "https://github.com/jazzjackrabbit/ihistory/releases/download/v#{version}/ihistory-aarch64-apple-darwin.tar.gz"
-      sha256 '180edd2c366d059c50e6e42ca48e3713e8b8930e8b5be100de87db56b5ebec01'
+      sha256 'ee3e49b041a47bbb499b530fab593e581028de56ac37a12e962093af06fe881a'
     end
 
     on_intel do
       url "https://github.com/jazzjackrabbit/ihistory/releases/download/v#{version}/ihistory-x86_64-apple-darwin.tar.gz"
-      sha256 'f068efcef89853aa9945f2f1be6b6ac08bfac00d02b2eb5a708afd3bca687818'
+      sha256 '1e7d9829e32785e3718d109e13e122774d30d175d8012e5cc9aa159938151374'
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/jazzjackrabbit/ihistory/releases/download/v#{version}/ihistory-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 '4038e305cbe858e43883430bc52eec881fb3e709341f520f47d0e02ca5b8cbdb'
+      sha256 '8f06383321e1d9d6c56ddf585d9b1881b4387383301e1a95165c439877014733'
     end
 
     on_intel do
       url "https://github.com/jazzjackrabbit/ihistory/releases/download/v#{version}/ihistory-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 '6a6b091f675128428a9130f0b4bbb50ab326c60ad43d50fd3b01438d7cd5a088'
+      sha256 '4a2467062825d15b4022584d789441ee18518087b5ff392005e1d6ccd400f629'
     end
   end
 
